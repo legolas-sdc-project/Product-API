e3195cc14fd3977b186d879826b41f624e44d371
Eun Ji Roh <eunjiroh@gmail.com>
2021-11-17 15:20:09 -0800

    added tables to schemas.sql file

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Product-API
+
+## Set up database
+ - start postgres
+
+  ```bash
+  sudo service postgresql start
+  ```
+
+ - check if postgres is connected and running
+
+  ``` bash
+  pgrep -u postgres -fa -- -D
+  ```
+
+ - to log into postgres server and enter shell
+
+  ```bash
+  sudo -u postgres psql
+  ```
+- to access database
+  ```bash
+  \c <database name>
+  ```
+
+when you make an index - 0(n) look up
+index whatever you are querying the data
+index based on that key
+make a leaf nodes in key
+grab the leaf nodes (the columns you want) and get data constant time
---
+++ database/schemas.sql
@@ -0,0 +1,45 @@
+-- create tables
+CREATE TABLE products (
+  product_id SERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  slogan VARCHAR(150),
+  description TEXT,
+  category VARCHAR(30),
+  default_price INT
+);
+
+CREATE TABLE features (
+  feature_id SERIAL NOT NULL PRIMARY KEY,
+  product_id INT REFERENCES products(product_id),
+  feature VARCHAR(50),
+  value VARCHAR(50)
+);
+
+CREATE TABLE styles (
+  style_id SERIAL NOT NULL PRIMARY KEY,
+  product_id INT REFERENCES products(product_id),
+  name VARCHAR(30) NOT NULL,
+  sale_price SMALLINT,
+  original_price INT,
+  default_style BOOLEAN
+);
+
+CREATE TABLE photos (
+  photo_id SERIAL NOT NULL PRIMARY KEY,
+  style_id INT REFERENCES styles(style_id),
+  thumbnail_url TEXT,
+  url TEXT
+);
+
+CREATE TABLE skus (
+  sku_id SERIAL NOT NULL PRIMARY KEY,
+  style_id INT REFERENCES styles(style_id),
+  quantity SMALLINT,
+  value VARCHAR(5)
+);
+
+CREATE TABLE related (
+  related_id SERIAL NOT NULL PRIMARY KEY,
+  current_product_id INT,
+  related_product_id INT
+);
