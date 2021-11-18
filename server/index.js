d4c7b075f66245b095c1a1949e181a9f35270b07
Eun Ji Roh <eunjiroh@gmail.com>
2021-11-18 14:08:45 -0800

    set up routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const router = require('./router');
+const db = require('../database/index');
 
 const app = express();
 
@@ -8,8 +8,27 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
-// Set up our routes
-app.use('/api', router);
+// Set up routes
+app.get('/products', (req, res) => {
+  const count = req.query.count || 5;
+  const page = req.query.page || 1;
+  db.getAllProducts(count, page);
+});
+
+app.get('/products/:product_id', (req, res) => {
+  const productID = req.params.product_id;
+  db.getProduct(productID);
+});
+
+app.get('/products/:product_id/styles', (req, res) => {
+  const productID = req.params.product_id;
+  db.getProductInfo(productID);
+});
+
+app.get('/products/:product_id/related', (req, res) => {
+  const productID = req.params.product_id;
+  db.getRelatedInfo(productID);
+});
 
 // Run server
 app.listen(3000, () => {
--- server/routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-
-router.get('/products');
-router.get('/products/:product_id');
-router.get('/products/:product_id/styles');
-router.get('/products/:product_id/related');
