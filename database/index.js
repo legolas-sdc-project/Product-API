be3be7212835916f697594ab303eb99cb14cd4c1
Eun Ji Roh <eunjiroh@gmail.com>
2021-11-18 14:46:13 -0800

    query to getAllProducts

--- database/index.js
+++ database/index.js
@@ -8,28 +8,37 @@ const pool = new Pool({
   port: 5432,
 });
 
-module.exports.getAllProducts = async (count, page) => {
+pool.connect()
+  .then(() => console.log('Connected to database'))
+  .catch((err) => console.log(err));
+
+module.exports.getAllProducts = async (count, page, cb) => {
+  const low = (page - 1) * count + 1;
+  const high = page * count;
+  const query = {
+    text: 'SELECT * FROM products WHERE product_id BETWEEN $1 and $2',
+    values: [low, high],
+  };
+  await pool.query(query)
+    .then((results) => cb(null, results))
+    .catch((err) => cb(err));
+};
+
+module.exports.getProduct = async (productID, cb) => {
   const query = {};
   await pool.query(query)
     .then()
     .catch();
 };
 
-module.exports.getProduct = async (productID) => {
+module.exports.getProductInfo = async (productID, cb) => {
   const query = {};
   await pool.query(query)
     .then()
     .catch();
 };
 
-module.exports.getProductInfo = async (productID) => {
-  const query = {};
-  await pool.query(query)
-    .then()
-    .catch();
-};
-
-module.exports.getRelatedInfo = async (productID) => {
+module.exports.getRelatedInfo = async (productID, cb) => {
   const query = {};
   await pool.query(query)
     .then()
