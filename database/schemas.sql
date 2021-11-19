4c88a7a51c1ef1828d1901ba666cff15f503674e
Eun Ji Roh <eunjiroh@gmail.com>
2021-11-18 17:32:17 -0800

    index database for faster queries

--- database/schemas.sql
+++ database/schemas.sql
@@ -43,3 +43,11 @@ CREATE TABLE related (
   current_product_id INT,
   related_product_id INT
 );
+
+-- create indexes
+CREATE INDEX product_index ON products (product_id);
+CREATE INDEX feature_index ON features (product_id);
+CREATE INDEX style_index ON styles (product_id);
+CREATE INDEX photo_ndex ON photos (style_id);
+CREATE INDEX sku_index ON skus (style_id);
+CREATE INDEX related_index ON related (current_product_id);
