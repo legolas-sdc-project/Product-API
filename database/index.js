7cd08eec7664d3a4775c3bc5d31b4edc3f90981e
Eun Ji Roh <eunjiroh@gmail.com>
2021-11-30 08:41:36 -0800

    local load testing

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   host: 'localhost',
   user: 'postgres',
   database: 'atelierproducts',
-  password: 'root',
+  password: '',
   port: 5432,
 });
 
--- testing/loadTesting.js
+++ testing/loadTesting.js
@@ -2,14 +2,14 @@ const http = require('k6/http');
 const { sleep, check } = require('k6');
 
 export const options = {
-  vus: 1000,
-  duration: '1s',
+  vus: 1,
+  duration: '10s',
 };
 
-const url = 'http://localhost:3010/products/';
+const url = 'http://localhost:3010/products';
 
 export default function () {
-  const res = http.get(`${url}`);
+  const res = http.get(`${url}/100000/related`);
   check(res, {
     'status was 200': r => r.status === 200,
     'transaction time < 2000ms': r => r.timings.duration < 2000,
