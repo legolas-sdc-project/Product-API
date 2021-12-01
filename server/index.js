b9cbd79194a92a00d35a842eed0f7d7a734b9227
Eun Ji Roh <eunjiroh@gmail.com>
2021-11-30 16:36:15 -0800

    loader verification

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 // const morgan = require('morgan');
 const cors = require('cors');
+const fs = require('fs');
 const db = require('../database/index');
 
 const app = express();
@@ -10,6 +11,12 @@ const app = express();
 // app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
+
+// route to verify loader
+app.use('/loaderio-3e0794d9436238afb376f1b6f3e8016b', (req, res) => {
+  const loaderIOfile = fs.readFileSync('../loaderio-3e0794d9436238afb376f1b6f3e8016b.txt', 'utf-8');
+  res.send(loaderIOfile);
+});
 
 // Set up routes
 app.get('/products', async (req, res) => {
